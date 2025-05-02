02ac8b65d818f0bc471e59df0dea7031f948208b
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-02 13:56:37 +0300

    prepare supplier minim controllers

---
+++ src/main/java/itu/eval_2/newapp/controllers/QuotationController.java
@@ -0,0 +1,11 @@
+package itu.eval_2.newapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public record QuotationController() {
+    // TODO : Afficher page list de quotation pour un fournisseur
+    // TODO : Afficher page de detail quotation
+    // TODO : Modification prix quotation
+    // TODO : Validataion quotation
+}
---
+++ src/main/java/itu/eval_2/newapp/controllers/SupplierController.java
@@ -0,0 +1,9 @@
+package itu.eval_2.newapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SupplierController {
+    // TODO: afficher page list de supplier
+    // TODO: Ajouter Fournisseur dans navbar
+}
