f6a04f9d053ecde5b38fde766cd0b1980ea9b4f0
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-24 05:27:33 +0300

    create User Role Models

---
+++ src/main/java/itu/eval_2/newapp/models/user/UserRole.java
@@ -0,0 +1,28 @@
+package itu.eval_2.newapp.models.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import itu.eval_2.newapp.models.action.FrappeDocument;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UserRole extends FrappeDocument {
+
+    private  String role;
+    private String parent;
+    @JsonProperty("parentfield")
+    private String parentField;
+    @JsonProperty("parenttype")
+    private String parentType;
+
+    public UserRole(){
+        super("Has Role");
+    }
+
+    @Override
+    public void save_controle() {}
+
+    @Override
+    public void update_cotnrole() {}
+}
